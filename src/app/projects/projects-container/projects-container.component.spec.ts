80a09166151ab7df112c42a4f7c3841eae141235
Craig McKeachie <craigdmckeachie@gmail.com>
2021-01-05 12:19:34 -0500

    updated to Angular 11

--- src/app/projects/projects-container/projects-container.component.spec.ts
+++ src/app/projects/projects-container/projects-container.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ProjectsContainerComponent } from './projects-container.component';
 
@@ -6,7 +6,7 @@ describe('ProjectsContainerComponent', () => {
   let component: ProjectsContainerComponent;
   let fixture: ComponentFixture<ProjectsContainerComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ProjectsContainerComponent ]
     })
