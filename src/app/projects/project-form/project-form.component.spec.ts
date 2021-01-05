56657522fde0bae9eef8642b6e040df1f963a88a
Craig McKeachie <craigdmckeachie@gmail.com>
2021-01-05 12:11:14 -0500

    updated to Angular 11

--- src/app/projects/project-form/project-form.component.spec.ts
+++ src/app/projects/project-form/project-form.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ProjectFormComponent } from './project-form.component';
 
@@ -6,7 +6,7 @@ describe('ProjectFormComponent', () => {
   let component: ProjectFormComponent;
   let fixture: ComponentFixture<ProjectFormComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ProjectFormComponent ]
     })
