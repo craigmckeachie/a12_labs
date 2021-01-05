14412e34be9f0faa066caa1adf3676f41ea917f0
Craig McKeachie <craigdmckeachie@gmail.com>
2021-01-05 11:58:30 -0500

    updated to Angular 11

--- src/app/shared/validation-errors/validation-errors.component.spec.ts
+++ src/app/shared/validation-errors/validation-errors.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ValidationErrorsComponent } from './validation-errors.component';
 
@@ -6,7 +6,7 @@ describe('ValidationErrorsComponent', () => {
   let component: ValidationErrorsComponent;
   let fixture: ComponentFixture<ValidationErrorsComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ValidationErrorsComponent ]
     })
